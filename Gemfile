2cb515626ca9cd6dd211e869942e54c9a316259b
Shane Stahlheber <shane.stahlheber@gmail.com>
2015-10-26 11:17:35 -0700

    trying out coffeescript

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'github-pages'
 gem 'sass'
+gem 'coffee-script-source', '1.8.0'
--- demo/SyrupDemo.js
+++ demo/SyrupDemo.js
@@ -1,5 +1,5 @@
 /*!
- * Syrup Demo JS v0.1a
+ * Syrup Demo JS v0.2.1
  * by Shane Stahlheber
  *
  * Public Domain
@@ -8,10 +8,9 @@
  */
 
 function SyrupDemo(opts) {
-	this.WIDTH = 200;
-	this.polarize = true;
-
-	this.bimage = new Array(this.WIDTH);
+    this.WIDTH = 200;
+    this.polarize = true;
+    this.bimage = new Array(this.WIDTH);
 }
 
 SyrupDemo.Polarizer = function (v, angle) {
@@ -27,76 +26,69 @@ SyrupDemo.Polarizer = function (v, angle) {
 }
 
 SyrupDemo.Rotator = function (v, phase) {
-	var cos = new Complex(Math.cos(phase), 0);
-	var sin = new Complex(Math.sin(phase), 0);
-	return new CVec([Complex.add(
-						Complex.mul(v.x(), cos),
-						Complex.mul(v.y(), sin)),
-					Complex.sub(
-						Complex.mul(v.x(), sin),
-						Complex.mul(v.y(), cos))]);
+    var cos = new Complex(Math.cos(phase), 0);
+    var sin = new Complex(Math.sin(phase), 0);
+    return new CVec([Complex.add(
+                        Complex.mul(v.x(), cos),
+                        Complex.mul(v.y(), sin)),
+                    Complex.sub(
+                        Complex.mul(v.x(), sin),
+                        Complex.mul(v.y(), cos))]);
 }
 
 SyrupDemo.prototype.DoSimulation = function (rotation, polangle) {
-	var sample = 0.0;
-	
-	// Initial Wave (Horizontally Polarized)
-    var norm = 1.0 / Math.sqrt(2.0);
+    // Initial Wave (Horizontally Polarized)
     var H = new CVec([new Complex(1, 0), new Complex(0, 0)])
 
-	// Rotate Wave
+    // Rotate Wave
     H = SyrupDemo.Rotator(H, rotation);
 
-	// Final Polarizer (Vertical Polarizer)
-	if (this.polarize)
-	{
+    // Final Polarizer (Vertical Polarizer)
+    if (this.polarize)
         H = SyrupDemo.Polarizer(H, polangle);
-	}
 
     // get the intensity
-    sample = Math.sqrt(H.y().re*H.y().re + H.y().im*H.y().im
-                     + H.x().re*H.x().re + H.x().im*H.x().im);
-	
-	return sample;
+    return Math.sqrt(H.y().re*H.y().re + H.y().im*H.y().im
+                   + H.x().re*H.x().re + H.x().im*H.x().im);
 }
 
 SyrupDemo.prototype.update = function (ctx, angle, polfilter, redfilter, greenfilter, bluefilter) {
 
-	this.polarize = polfilter;
-	
-	// calculate buffer values
-	for (var x = 0; x < this.WIDTH; x++)
-	{
-		// radius of the bottle
-		var radius = this.WIDTH/6.0;
+    this.polarize = polfilter;
+    
+    // calculate buffer values
+    for (var x = 0; x < this.WIDTH; x++)
+    {
+        // radius of the bottle
+        var radius = this.WIDTH/6.0;
 
-		// default background settings
-		var distance = 0.0;
-		var brightness = 1.0;
-		var rb = 1.0;
-		var gb = 1.0;
-		var bb = 1.0;
+        // default background settings
+        var distance = 0.0;
+        var brightness = 1.0;
+        var rb = 1.0;
+        var gb = 1.0;
+        var bb = 1.0;
 
-		// Bottle boundaries
-		if (x > 2.0*radius && x < 4.0*radius)
-		{
+        // Bottle boundaries
+        if (x > 2.0*radius && x < 4.0*radius)
+        {
             // find the distance to travel through the syrup
-			var xc = x - 3.0*radius;
-			distance = 2.0*Math.sqrt(radius*radius - xc*xc);
-			
-			// color of the syrup (yellowish and darker around edges)
+            var xc = x - 3.0*radius;
+            distance = 2.0*Math.sqrt(radius*radius - xc*xc);
+            
+            // color of the syrup (yellowish and darker around edges)
             // this is for aesthetic purposes
             brightness = distance / (2.0*radius);
-			rb = 0.9;
-			gb = 0.9;
-			bb = 0.72;
-		}
+            rb = 0.9;
+            gb = 0.9;
+            bb = 0.72;
+        }
 
-		// The distance traveled through the syrup (2.5 inches diameter)
-		distance *= 2.5/(2.0*radius);
+        // The distance traveled through the syrup (2.5 inches diameter)
+        distance *= 2.5/(2.0*radius);
 
-		// Calculate colors
-		//  - Rotation is dependent on the distance traveled through the syrup.
+        // Calculate colors
+        //  - Rotation is dependent on the distance traveled through the syrup.
 
         var delta_n = 3.87e-6;
         var distance_nm = distance * 2.54e+7;
@@ -117,23 +109,23 @@ SyrupDemo.prototype.update = function (ctx, angle, polfilter, redfilter, greenfi
         blue += this.DoSimulation(distance_nm * Math.PI * delta_n / 470.0, angle);
         blue /= 3.0;
 
-		if (!redfilter)
-			red *= 0;
-		if (!greenfilter)
-			green *= 0;
-		if (!bluefilter)
-			blue *= 0;
+        if (!redfilter)
+            red *= 0;
+        if (!greenfilter)
+            green *= 0;
+        if (!bluefilter)
+            blue *= 0;
 
-		red *= brightness*rb;
-		green *= brightness*gb;
-		blue *= brightness*bb;
+        red *= brightness*rb;
+        green *= brightness*gb;
+        blue *= brightness*bb;
 
-		// Draw vertical bars
-		var barwidth = 200/this.WIDTH;
-		ctx.fillStyle = "rgb(" + Math.floor(255*red) + "," +
-								 Math.floor(255*green) + "," +
-								 Math.floor(255*blue) + ")";
-		ctx.fillRect(x*barwidth, 0, x*barwidth+barwidth, 16);
-	}
+        // Draw vertical bars
+        var barwidth = 200/this.WIDTH;
+        ctx.fillStyle = "rgb(" + Math.floor(255*red) + "," +
+                                 Math.floor(255*green) + "," +
+                                 Math.floor(255*blue) + ")";
+        ctx.fillRect(x*barwidth, 0, x*barwidth+barwidth, 16);
+    }
 }
 
